1ae14ea32de51d5a29c32bc1974c79d436a62b78
David Auza <davidauza01@gmail.com>
2021-08-22 17:14:52 -0500

    Variables and Data Types

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
+'use strict'
+
 console.log('Hello World');
+let x = 'Hello';
+x = 2;
+x = false;
+console.log(x);
+const y = 'David';
+// console.log(x + y);
+console.log('She sells sea shells\non the sea shore');
+var z;
+console.log(z);
+let a = 'my constant';
+a = 'something else';
+console.log(a);
+console.log(x + ' ' + y);
+console.log(x.length);
+a = 10;
+const b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+const c = 5;
+console.log((a + b) * c);
+console.log(a > b);
+console.log(a < b);
+let p = true;
+const q = false;
+console.log(p || q);
+console.log(!p);
+let k;
+console.log(k);
+p = undefined;
+console.log(p);
+let student = {};
+student.firstname = 'David';
+student.lastname = 'Auza';
+student.age = 35;
+student = {
+    firstname: 'David',
+    lastname: 'Auza',
+    age: 35
+};
+console.log(student.firstname);
