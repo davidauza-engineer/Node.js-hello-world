d67ca17c4d8fc82d4d1084bc43f020e51d3febfa
David Auza <davidauza01@gmail.com>
2021-08-22 17:22:08 -0500

    Add arrays.js

---
+++ arrays.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let x = [10, 20, 30];
+let y = [10, 20, 'David', true];
+console.log(x[0]);
+console.log(y[2]);
+console.log(x.length);
+x[2] = 40;
+x.push(50, 60);
+x.pop();
+console.log(x);
